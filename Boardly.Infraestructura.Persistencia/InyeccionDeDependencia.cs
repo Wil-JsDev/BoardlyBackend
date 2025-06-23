32789a5126ae72cd9aacdf41e57c3a5be377e928
Dayron <Dayronbp06@gmail.com>
2025-06-23 14:29:09 -0400

    feat: add api infrastructure

--- Boardly.Infraestructura.Persistencia/InyeccionDeDependencia.cs
+++ Boardly.Infraestructura.Persistencia/InyeccionDeDependencia.cs
@@ -1,5 +1,31 @@
-﻿namespace Boardly.Infraestructura.Persistencia;
+﻿using Boardly.Infraestructura.Persistencia.Contexto;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class Class1
+namespace Boardly.Infraestructura.Persistencia;
+
+public static class InyeccionDeDependencia
 {
+    public static void AgregarPersistencia(this IServiceCollection servicio, IConfiguration configuracion)
+    {
+        
+        #region DbContexto
+
+        servicio.AddDbContext<BoardlyContexto>(postgres =>
+        {
+            postgres.UseNpgsql(configuracion.GetConnectionString("TrivoBackend"), b =>
+            {
+                b.MigrationsAssembly("Boardly.Infraestructura.Persistencia");
+            });
+        });
+
+        #endregion
+        
+        #region ID
+        
+        #endregion
+
+    }
+    
 }
