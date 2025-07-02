aaa2ac0474b9dc3890b193a7792d81f85a747a98
Dayron <Dayronbp06@gmail.com>
2025-07-01 20:42:08 -0400

    feat: add employee and ceo controllers

--- Boardly.Infraestructura.Api/Controllers/V1/UsuarioControlador.cs
+++ Boardly.Infraestructura.Api/Controllers/V1/UsuarioControlador.cs
@@ -1,6 +1,10 @@
 using Asp.Versioning;
+using Boardly.Aplicacion.DTOs.Ceo;
+using Boardly.Aplicacion.DTOs.Empleado;
 using Boardly.Aplicacion.DTOs.Usuario;
+using Boardly.Dominio.Puertos.CasosDeUso.Ceo;
 using Boardly.Dominio.Puertos.CasosDeUso.Codigo;
+using Boardly.Dominio.Puertos.CasosDeUso.Empleado;
 using Boardly.Dominio.Puertos.CasosDeUso.Usuario;
 using Boardly.Dominio.Utilidades;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +16,8 @@ namespace Boardly.Infraestructura.Api.Controllers.V1;
 [Route("api/v{version:apiVersion}/user")]
 public class UsuarioControlador(
     ICrearUsuario<CrearUsuarioDto, UsuarioDto> crearUsuario,
+    ICrearCeo<CrearCeoDto, CeoDto> crearCeo,
+    ICrearEmpleado<CrearEmpleadoDto, EmpleadoDto> crearEmpleado,
     IConfirmarCuenta<Resultado> confirmarCuenta
 ) : ControllerBase
 {
@@ -24,6 +30,28 @@ public class UsuarioControlador(
             return Ok(resultado.Valor);
 
         return BadRequest(resultado.Error);
+    }
+    
+    [HttpPost("ceo")]
+    public async Task<IActionResult> RegistrarCeo([FromQuery] CrearCeoDto solicitud, CancellationToken cancellationToken)
+    {
+        var resultado = await crearCeo.CrearCeoAsync(solicitud, cancellationToken);
+        if (resultado.EsExitoso) 
+            return Ok(resultado.Valor);
+        
+        return BadRequest(resultado.Error);
+    }
+
+    [HttpPost("empleado")]
+    public async Task<IActionResult> CrearEmpleado([FromQuery] CrearEmpleadoDto solicitud,
+        CancellationToken cancellationToken)
+    {
+        var empleado = await crearEmpleado.CrearEmpleadoAsync(solicitud, cancellationToken);
+        if (empleado.EsExitoso)
+        {
+            return Ok(empleado.Valor);
+        }
+        return BadRequest(empleado.Error);
     }
 
     [HttpPost("confirm-account")]
