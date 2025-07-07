0c740a8b90a9530071493a8f883195dd20e1b07f
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-07-06 21:15:01 -0400

    refactor: rename parameter for clarity

--- Boardly.Dominio/Puertos/Repositorios/Cuentas/IEmpresaRepositorio.cs
+++ Boardly.Dominio/Puertos/Repositorios/Cuentas/IEmpresaRepositorio.cs
@@ -5,6 +5,6 @@ namespace Boardly.Dominio.Puertos.Repositorios.Cuentas;
 public interface IEmpresaRepositorio: IGenericoRepositorio<Empresa>
 {
     Task<bool> ExisteNombreEmpresaAsync(string nombreEmpresa, CancellationToken cancellationToken);
-    Task<bool> NombreEmpresaEnUso(string nombreUsuario, Guid empresaId, CancellationToken cancellationToken);
+    Task<bool> NombreEmpresaEnUso(string nombreEmpresa, Guid empresaId, CancellationToken cancellationToken);
 
 }
