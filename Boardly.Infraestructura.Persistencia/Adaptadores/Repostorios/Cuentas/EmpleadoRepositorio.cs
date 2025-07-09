8a94bb4756559d1471e4fe03c76e7e8f6a5496de
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-07-08 21:30:43 -0400

    feat: add method get employee by company id

--- Boardly.Infraestructura.Persistencia/Adaptadores/Repostorios/Cuentas/EmpleadoRepositorio.cs
+++ Boardly.Infraestructura.Persistencia/Adaptadores/Repostorios/Cuentas/EmpleadoRepositorio.cs
@@ -1,10 +1,18 @@
 using Boardly.Dominio.Modelos;
 using Boardly.Dominio.Puertos.Repositorios.Cuentas;
 using Boardly.Infraestructura.Persistencia.Contexto;
+using Microsoft.EntityFrameworkCore;
 
 namespace Boardly.Infraestructura.Persistencia.Adaptadores.Repostorios.Cuentas;
 
 public class EmpleadoRepositorio(BoardlyContexto boardlyContexto) : GenericoRepositorio<Empleado>(boardlyContexto), IEmpleadoRepositorio
 {
-    
+    public async Task<IEnumerable<Empleado>> ObtenerPorEmpresaIdAsync(Guid empresaId, CancellationToken cancellationToken)
+    {
+        return await _boardlyContexto.Set<Empleado>()
+            .AsNoTracking()
+            .Include(e => e.Usuario)
+            .Where(e => e.EmpresaId == empresaId)
+            .ToListAsync(cancellationToken);
+    }
 }
