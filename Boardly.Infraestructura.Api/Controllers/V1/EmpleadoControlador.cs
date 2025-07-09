d870d91405ce2fc0267f929004e76486d6432efe
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-07-08 21:32:49 -0400

    feat: add endpoint

--- Boardly.Infraestructura.Api/Controllers/V1/EmpleadoControlador.cs
+++ Boardly.Infraestructura.Api/Controllers/V1/EmpleadoControlador.cs
@@ -1,7 +1,5 @@
 using Asp.Versioning;
-using Boardly.Aplicacion.DTOs.Ceo;
 using Boardly.Aplicacion.DTOs.Empleado;
-using Boardly.Dominio.Puertos.CasosDeUso.Ceo;
 using Boardly.Dominio.Puertos.CasosDeUso.Empleado;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +9,8 @@ namespace Boardly.Infraestructura.Api.Controllers.V1;
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/employee")]
 public class EmpleadoControlador(
-    ICrearCeo<CrearCeoDto, CeoDto> crearCeo,
-    ICrearEmpleado<CrearEmpleadoDto, EmpleadoDto> crearEmpleado
+    ICrearEmpleado<CrearEmpleadoDto, EmpleadoDto> crearEmpleado,
+    IObtenerIdEmpleado<EmpleadoResumenDto> obtenerEmpleado
     ) : ControllerBase
 {
     [HttpPost]
@@ -23,5 +21,15 @@ public class EmpleadoControlador(
             return Ok(empleado.Valor);
              
         return BadRequest(empleado.Error);
+    }
+
+    [HttpGet("companies/{companyId}")]
+    public async Task<IActionResult> ObtenerIdCeo([FromRoute] Guid companyId, CancellationToken cancellationToken)
+    {
+        var resultado = await obtenerEmpleado.ObtenerIdEmpleadoAsync(companyId, cancellationToken);
+        if (resultado.EsExitoso)
+            return Ok(resultado.Valor);
+        
+        return BadRequest(resultado.Error);
     }
 }
