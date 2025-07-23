7e452ad6bb4061ad19fa5e945ebe00ebfe30b47b
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-07-22 23:48:56 -0400

    feat: add employee id parameter

--- Boardly.Aplicacion/DTOs/Tarea/CrearTareaDto.cs
+++ Boardly.Aplicacion/DTOs/Tarea/CrearTareaDto.cs
@@ -3,6 +3,7 @@ namespace Boardly.Aplicacion.DTOs.Tarea;
 public record CrearTareaDto(
     Guid ProyectoId,
     Guid UsuarioId,
+    Guid EmpleadoId,
     string Titulo,
     string? Descripcion,
     DateTime FechaVencimiento,
