65f325b272ba6a4504c4c7f45d5e38ec6758ac1e
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-06 00:41:08 -0400

    feat: add migrations

---
+++ Boardly.Infraestructura.Persistencia/Migrations/20250806041047_AgregarEnRevisionPropiedad.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Boardly.Infraestructura.Persistencia.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarEnRevisionPropiedad : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EnRevision",
+                table: "Tarea",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnRevision",
+                table: "Tarea");
+        }
+    }
+}
